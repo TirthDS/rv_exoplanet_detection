db13254cbaf9d17c0c6a15cb6dd444bb65b28dc3
TirthDS <TirthDS@users.noreply.github.com>
2022-04-29 00:44:56 -0700

    Add files via upload

---
+++ data/real_data/obtain_exo_rv.py
@@ -0,0 +1,70 @@
+'''
+Parse through NASA's exoplanet archive contributed RV dataset
+for radial velocity time-series data of exoplanetary systems.
+Contents are in 'dataset' directory.
+'''
+import numpy as np
+import glob
+import re
+import pickle
+
+def parse_files(all_files):
+    all_exo_data_nasa = []
+    date_units = []
+    rv_units = []
+
+    # Cannot use pandas due to slight variations in data file formats
+    for file in all_files:
+        file1 = open(file, 'r')
+        lines = file1.readlines()
+        
+        # Search for the index of the beginning of RVs
+        beginning_index = [idx for idx, s in enumerate(lines) if '|' in s][-1] + 1
+
+        # Get index of the date units:
+        date_id_index = [idx for idx, s in enumerate(lines) if 'DATE_UNITS' in s][0]
+
+        # Get index of the rv units:
+        rv_id_index = [idx for idx, s in enumerate(lines) if 'VALUE_UNITS' in s][0]
+
+        char = "\""
+        date_type_idx = [i.start() for i in re.finditer(char, lines[date_id_index])]
+        if (len(date_type_idx) == 0):
+            char = "\'"
+            date_type_idx = [i.start() for i in re.finditer(char, lines[date_id_index])]
+        
+        rv_type_idx = [i.start() for i in re.finditer(char, lines[rv_id_index])]
+
+        date_type = lines[date_id_index][date_type_idx[0]+1:date_type_idx[1]].lower().replace(' ', '')
+        rv_type = lines[rv_id_index][rv_type_idx[0]+1:rv_type_idx[1]].lower().replace(' ', '')
+        
+        # Grab rvs and remove all 'nan' values
+        rv = lines[beginning_index:]
+        rvs = np.array([item.split() for item in rv]).astype(float)
+        rvs = rvs[~np.isnan(rvs).any(axis=1)].T
+
+        dates = rvs[0]
+        rvs = rvs[1]
+
+        if (len(dates) == 0):
+            continue
+
+        date_units.append(date_type)
+        rv_units.append(rv_type)
+
+        data = np.stack((dates, rvs))
+        all_exo_data_nasa.append(data)
+        
+        return all_exo_data_nasa, date_units, rv_units
+
+if __name__ == '__main__':
+    all_files = glob.glob('dataset/*')
+    all_exo_data_nasa, date_units, rv_units = parsefiles(all_files)
+    
+    # Ensure consistency of units for rv measurements
+    assert(np.all([item=='m/s' for item in rv_units]))
+    assert(np.all([item=='days' for item in date_units]))
+    
+    # Save the extracted data
+    with open('all_exo_data_nasa', 'wb') as f:
+        pickle.dump(all_exo_data_nasa, f)
---
+++ data/real_data/request_non_exo_rv.py
@@ -0,0 +1,83 @@
+'''
+Retrieve radial velocity time-series data from DACE database of systems
+not known to have any exoplanets.
+'''
+
+import numpy as np
+from dace.spectroscopy import Spectroscopy
+from dace.exoplanet import Exoplanet
+import pickle
+import itertools
+
+def get_all_exoplanet_names():
+    exoplanet_data = Exoplanet.query_database(limit = 50000)
+    pl_names = exoplanet_data['obj_id_catname']
+    
+    all_exoplanets = set()
+    for name in pl_names:
+        if (name[-1].islower()):
+            name = name[:-2].replace(' ', '').lower()
+        else:
+            name = name.replace(' ', '').lower()
+
+        name = name.replace('-', '')
+        all_exoplanets.add(name)
+        
+    return all_exoplanets
+
+def get_all_non_exoplanet_names(all_exoplanets):
+    # Get the names of all systems with spectroscopic data
+    observedTargets = None
+    with open('observed_targets_data','rb') as f: 
+        observedTargets = pickle.load(f)
+    
+    names = observedTargets['obj_id_catname']
+    processed_names = [i.replace('-', '').lower() for i in names]
+    ids = observedTargets['obj_id_daceid']
+    dictionary = dict(zip(processed_names, ids))
+    
+    # Keep track of all the systems with IDs corresponding to exoplanets
+    all_exoplanet_ids = set()
+    for item in all_exoplanets:
+        try:
+            dace_id = dictionary[item]
+            all_exoplanet_ids.add(dace_id)
+        except KeyError:
+            continue
+    
+    # Get all the unique systems that do not contain exoplanets
+    non_exoplanet_names = set()
+    seen_ids = set()
+    for (name, dace_id) in itertools.zip_longest(names, ids):
+        if dace_id in all_exoplanet_ids or dace_id in seen_ids:
+            continue
+        else:
+            non_exoplanet_names.add(name)
+            seen_ids.add(dace_id)
+    
+    return non_exoplanet_names
+
+def request_rv_data(all_non_exoplanet_names):
+    all_rv_non_exoplanet_data = []
+    count = 0
+    for name in all_non_exoplanet_names:
+        count += 1
+        spectro_time_series = Spectroscopy.get_timeseries(name, sorted_by_instrument=False)
+        dates = spectro_time_series['rjd']
+        
+        if (len(dates) == 0):
+            continue
+        
+        rvs = spectro_time_series['rv']
+        data = np.stack((dates, rvs))
+        all_rv_non_exoplanet_data.append(data)
+        print(count)
+    
+    with open ("all_non_exo_data", "wb") as fp:
+        pickle.dump(all_rv_non_exoplanet_data, fp)
+    
+
+if __name__ == '__main__':
+    all_exoplanet_names = get_all_exoplanet_names()
+    all_non_exoplanet_names = get_all_non_exoplanet_names(all_exoplanet_names)
+    request_rv_data(all_non_exoplanet_names)
