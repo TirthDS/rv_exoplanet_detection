ffdbaa46167c9a27115b163d20ebb4c6172d651a
TirthDS <TirthDS@users.noreply.github.com>
2022-04-29 22:18:53 -0700

    Add files via upload

---
+++ data/real_data/request_all_rv_targets.py
@@ -0,0 +1,21 @@
+import pickle
+from dace.spectroscopy import Spectroscopy
+
+'''
+Request spectroscopic candidates with radial velocity data from DACE database.
+Save requests to a file (large).
+'''
+
+def request_spectroscopic_targets():
+    '''
+    Query 100,000 spectroscopic targets (most will be duplicates, but this ensures we get almost all unique targets)
+    and save results to pickle file. This file is accessed in request_non_exo_rv.py to remove duplicates and obtain
+    the radial velocity data for non-exoplanetary systems.
+    '''
+    
+    observedTargets = Spectroscopy.query_database(limit=100000)
+    
+    with open('observedTargets', 'wb') as fp:
+        fp.dump(observedTargets, fp)
+    
+    
