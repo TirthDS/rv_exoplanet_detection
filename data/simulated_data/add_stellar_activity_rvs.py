49750a95956a63dea8edf91b489ef185b91c585b
TirthDS <tirth.surti@gmail.com>
2022-05-19 17:30:58 -0700

    changed gaussian noise addition amount

--- data/simulated_data/add_stellar_activity_rvs.py
+++ data/simulated_data/add_stellar_activity_rvs.py
@@ -14,7 +14,7 @@ def exo_stellar_activity_rvs():
     '''
     Uses 10,000 of the randomly generated stellar activity radial velocity curves
     from SOAP 2.0 and adds them to the 10,000 exoplanet radial velocity curves. Applies
-    a Guassian error of 5 m/s to simulate instrumental error and other random stellar
+    a Guassian error of 2 m/s to simulate instrumental error and other random stellar
     activity.
     
     This function assumes that stellar rotation periods used for SOAP 2.0
@@ -27,7 +27,7 @@ def exo_stellar_activity_rvs():
        - List of files containing solar rvs injected into the exoplanetary rv
          systems.
     '''
-    stellar_rv_files = glob.glob('generated_data/solar_rvs/outputs/*') # Assume all solar rv curves in relative directory
+    stellar_rv_files = glob.glob('generated_data/outputs/*') # Assume all solar rv curves extracted from zip to relative directory
     
     with open('generated_data/simulated_rvs_no_solar', 'rb') as f: # Load file containing raw simulated rvs
         exo_rvs = pickle.load(f)
@@ -51,7 +51,7 @@ def exo_stellar_activity_rvs():
         data = np.array([item.split() for item in rvs]).astype(float)
         stellar_activity_rvs = data.T[2]
         exoplanet_rvs = exo_rvs[i][1]
-        new_rv = stellar_activity_rvs + exoplanet_rvs + (5 * np.random.randn(len(exoplanet_rvs)))
+        new_rv = stellar_activity_rvs + exoplanet_rvs + (2 * np.random.randn(len(exoplanet_rvs)))
         dates = exo_rvs[i][0]
 
         data = np.stack((dates, new_rv))
@@ -84,11 +84,11 @@ def non_exo_stellar_activity_rvs(stellar_rv_files_seen):
             f.close()
 
             data = np.array([item.split() for item in rvs]).astype(float)
-            stellar_activity_rvs = data.T[2] + (5 * np.random.randn(len(data.T[2])))
+            stellar_activity_rvs = data.T[2] + (2 * np.random.randn(len(data.T[2])))
             phases = data.T[0]
 
             # Convert phases to back to date times
-            rotation_period = float(file[27:-4])
+            rotation_period = float(file[17:-4])
             max_num_periods_to_extend_to = np.random.randint(2, 50)
             n = np.random.randint(1, max_num_periods_to_extend_to, size = len(phases)) 
             dates = (n * rotation_period) + (phases * rotation_period)
